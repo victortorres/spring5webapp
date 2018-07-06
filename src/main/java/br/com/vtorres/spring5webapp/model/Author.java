b3f9462a42fdb80e31d6fe648de1caddf92767cd
Victor Torres <victor.h.c.torres@gmail.com>
2018-07-06 21:44:54 +0100

    adicionaod equals

--- src/main/java/br/com/vtorres/spring5webapp/model/Author.java
+++ src/main/java/br/com/vtorres/spring5webapp/model/Author.java
@@ -62,4 +62,29 @@ public class Author {
     public void setId(Long id) {
         this.id = id;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
 }
